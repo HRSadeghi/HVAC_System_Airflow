6d37c4ef17740de353f2f0f821f167d6922c7392
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2023-05-22 22:46:05 +0330

    Add project files.

---
+++ HVAC_System_Airflow/Search/Run.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace HVAC_System_Airflow.Search
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Run : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiDocument = commandData.Application.ActiveUIDocument;
+            var document = uiDocument.Document;
+
+            var elementId = uiDocument.Selection.PickObject(ObjectType.Element, "Please select an element to start with");
+            if (elementId == null) return Result.Cancelled;
+
+
+            var selectedElement = document.GetElement(elementId);
+
+            try
+            {
+                var ats = Search.SearchForAirTerminals(document, selectedElement);
+                var total_airflow = Search.CalculateTotalAirFlow(ats);
+                TaskDialog.Show("TotalAirflow", $"No of AirTerminals: {ats.Count} \nTota Airflow: {total_airflow} L/S", TaskDialogCommonButtons.Ok);
+            }
+            catch 
+            {
+                TaskDialog.Show("Inappropriate element", "An inappropriate element has been selected. Please select an element of an HVAC system.", TaskDialogCommonButtons.Ok);
+            }
+
+            return Result.Succeeded;
+        }
+
+
+    }
+}
---
+++ HVAC_System_Airflow/Search/Search.cs
@@ -0,0 +1,167 @@
+﻿using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVAC_System_Airflow.Search
+{
+    static class Search
+    {
+        private static List<int> elements_ids;
+        private static List<Element> AirTerminals = new List<Element>();
+
+
+        public static List<Element> SearchForAirTerminals(Autodesk.Revit.DB.Document document, Element element)
+        {
+            AirTerminals = new List<Element>();
+            elements_ids = new List<int>();
+
+
+            var conectors = GetConnectors(element);
+            if (conectors != null)
+            {
+                //TaskDialog.Show("Simple Command", $"Simple element: {elementId}", TaskDialogCommonButtons.Ok);
+                foreach (var connector in conectors)
+                {
+                    _searchForAirTerminals(document, (Connector)connector);
+
+                }
+            }
+            else
+                throw new Exception();
+            return AirTerminals;
+
+        }
+
+
+        public static double CalculateTotalAirFlow(List<Element> AirTerminals)
+        {
+            double totalAirFlow = 0;
+            foreach (var element in AirTerminals)
+            {
+                var airFlow = element.get_Parameter(Autodesk.Revit.DB.BuiltInParameter.RBS_DUCT_FLOW_PARAM)?.AsDouble() ?? 0;
+                totalAirFlow += airFlow;
+            }
+
+            totalAirFlow *= 28.31684; // convert to l/s
+            totalAirFlow = Math.Round(totalAirFlow, 3);
+            return totalAirFlow;
+        }
+
+
+        private static void _searchForAirTerminals(Autodesk.Revit.DB.Document document, Connector connector)
+        {
+            List<Connector> connected_connectors = new List<Connector>();
+
+            try
+            {
+                var mepSystem = connector.MEPSystem;
+
+                if (null != mepSystem)
+                {
+                    if (connector.IsConnected == true)
+                    {
+                        var connectorSet = connector.AllRefs;
+                        var csi = connectorSet.ForwardIterator();
+                        while (csi.MoveNext())
+                        {
+                            var connected = csi.Current as Connector;
+                            if (null != connected)
+                            {
+                                // look for physical connections
+                                if (connected.ConnectorType == ConnectorType.End ||
+                                    connected.ConnectorType == ConnectorType.Curve ||
+                                    connected.ConnectorType == ConnectorType.Physical)
+                                {
+                                    if (connector.Owner.Id != connected.Owner.Id)
+                                    {
+                                        if (!elements_ids.Contains(connected.Owner.Id.IntegerValue))
+                                        {
+                                            elements_ids.Add(connected.Owner.Id.IntegerValue);
+
+
+                                            var el = document.GetElement(connected.Owner.Id);
+                                            if (el.Category.Name == "Air Terminals")
+                                            {
+                                                AirTerminals.Add(el);
+                                            }
+
+                                            var new_conectors = GetConnectors(el);
+                                            foreach (var c in new_conectors)
+                                            {
+                                                _searchForAirTerminals(document, (Connector)c);
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Autodesk.Revit.Exceptions.InvalidOperationException ex)
+            {
+                try
+                {
+                    var HostElementId = GetPropValue(connector.Owner, "HostElementId") as ElementId;
+
+                    var selectedElement = document.GetElement(HostElementId);
+                    SearchForAirTerminals(document, selectedElement);
+                }
+                catch (Exception ex2)
+                {
+                    Console.WriteLine(ex2.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+
+        private static ConnectorSet GetConnectors(Element e)
+        {
+            ConnectorSet connectors = null;
+
+            if (e is FamilyInstance)
+            {
+                MEPModel m = ((FamilyInstance)e).MEPModel;
+
+                if (null != m
+                  && null != m.ConnectorManager)
+                {
+                    connectors = m.ConnectorManager.Connectors;
+                }
+            }
+            else if (e is Wire)
+            {
+                connectors = ((Wire)e)
+                  .ConnectorManager.Connectors;
+            }
+            else if (e is MEPCurve)
+            {
+                connectors = ((MEPCurve)e)
+                      .ConnectorManager.Connectors;
+            }
+            return connectors;
+        }
+
+
+        private static bool HasProperty(this object obj, string propertyName)
+        {
+            return obj.GetType().GetProperty(propertyName) != null;
+        }
+
+        private static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    }
+}
